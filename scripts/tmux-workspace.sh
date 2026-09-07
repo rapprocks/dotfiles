9cb52c04d61acf72f33a16ac8d5b237772cae289
rapprocks <rapprocks@users.noreply.github.com>
2026-09-07 06:18:53 +0200

    2027

--- scripts/tmux-workspace.sh
+++ scripts/tmux-workspace.sh
@@ -2,8 +2,8 @@
 
 SESSION="rapprocks"
 
-DOTFILES_DIR="$HOME/.dotfiles"
-DOTCONFIG_DIR="$HOME/personal/git/dotfiles"  # optional arg, fallback to $HOME
+DOTFILES_DIR="$HOME/nixconf"
+DOTCONFIG_DIR="$HOME/.dotfiles"  # optional arg, fallback to $HOME
 
 # If session already exists, just attach
 tmux has-session -t "$SESSION" 2>/dev/null && {
@@ -12,12 +12,12 @@ tmux has-session -t "$SESSION" 2>/dev/null && {
 }
 
 # Create session with HOME as default (affects all future windows/panes)
-tmux new-session -d -s "$SESSION" -n dotfiles -c "$HOME"
+tmux new-session -d -s "$SESSION" -n dotconfig -c "$HOME"
 tmux send-keys -t "$SESSION:dotfiles" "cd $DOTFILES_DIR && clear" C-m
 #tmux new-session -d -s "$SESSION" -n dotfiles -c "$DOTFILES_DIR"
 
 # Second window
-tmux new-window -t "$SESSION" -n dotconfig -c "$DOTCONFIG_DIR"
+tmux new-window -t "$SESSION" -n dotfiles -c "$DOTCONFIG_DIR"
 
 # Third window
 tmux new-window -t "$SESSION" -n rando
